06cf1da01876e63e8e14a36860bdabc46c18761d
kondors1995 <normandija1945@gmail.com>
2023-09-07 17:18:10 +0300

    treewide: Remove perf critical API & its ussage
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1900,12 +1900,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_sig = current->signal;
-		else if (unlikely(!strncmp(filename->name,
-					   UDFPS_BIN_PREFIX,
-					   strlen(UDFPS_BIN_PREFIX)))) {
-			current->flags |= PF_PERF_CRITICAL;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
-		}
 	}
 
 	if (is_global_init(current->parent))
